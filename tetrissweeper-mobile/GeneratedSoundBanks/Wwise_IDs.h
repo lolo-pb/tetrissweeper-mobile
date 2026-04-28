757337c33d09de9c2f4388ae6958d6205b0e0d8b
lutiboca <lutibocalandro@gmail.com>
2026-04-27 22:17:25 -0400

    bolas musiquita beta

---
+++ tetrissweeper-mobile/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID HIGHINT = 3420765524U;
+        static const AkUniqueID LOWINT = 3325438800U;
+        static const AkUniqueID MEDINT = 2933035976U;
+        static const AkUniqueID START = 1281810935U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace INTENSITY
+        {
+            static const AkUniqueID GROUP = 2470328564U;
+
+            namespace STATE
+            {
+                static const AkUniqueID HIGHINT = 3420765524U;
+                static const AkUniqueID LOWINT = 3325438800U;
+                static const AkUniqueID MEDINT = 2933035976U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace INTENSITY
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID HIGHINT = 3420765524U;
+        static const AkUniqueID LOWINT = 3325438800U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MEDIUMINT = 1233031547U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ tetrissweeper-mobile/Wwise IDs.gd
@@ -0,0 +1,121 @@
+class_name AK
+
+class EVENTS:
+
+	const HIGHINT = 3420765524.0
+	const LOWINT = 3325438800.0
+	const MEDINT = 2933035976.0
+	const START = 1281810935.0
+
+	const _dict = {
+		"Highint": HIGHINT,
+		"Lowint": LOWINT,
+		"Medint": MEDINT,
+		"Start": START
+	}
+
+class STATES:
+
+	class INTENSITY:
+		const GROUP = 2470328564.0
+
+		class STATE:
+			const NONE = 748895195.0
+			const LOWINT = 3325438800.0
+			const MEDINT = 2933035976.0
+			const HIGHINT = 3420765524.0
+
+	const _dict = {
+		"Intensity": {
+			"GROUP": 2470328564.0,
+			"STATE": {
+				"None": 748895195.0,
+				"Lowint": 3325438800.0,
+				"Medint": 2933035976.0,
+				"Highint": 3420765524.0
+			}
+		}
+	}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const SS_AIR_FEAR = 1351367891.0
+	const SS_AIR_STORM = 3715662592.0
+	const SS_AIR_FREEFALL = 3002758120.0
+	const SS_AIR_FURY = 1029930033.0
+	const SS_AIR_SIZE = 3074696722.0
+	const SS_AIR_RPM = 822163944.0
+	const SS_AIR_PRESENCE = 3847924954.0
+	const SS_AIR_MONTH = 2648548617.0
+	const SS_AIR_TIMEOFDAY = 3203397129.0
+	const SS_AIR_TURBULENCE = 4160247818.0
+
+	const _dict = {
+		"SS_Air_Fear": SS_AIR_FEAR,
+		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Freefall": SS_AIR_FREEFALL,
+		"SS_Air_Fury": SS_AIR_FURY,
+		"SS_Air_Size": SS_AIR_SIZE,
+		"SS_Air_RPM": SS_AIR_RPM,
+		"SS_Air_Presence": SS_AIR_PRESENCE,
+		"SS_Air_Month": SS_AIR_MONTH,
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE
+	}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const NEW_SOUNDBANK = 4072029455.0
+
+	const _dict = {
+		"Init": INIT,
+		"New_SoundBank": NEW_SOUNDBANK
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087.0
+	const MUSIC = 3991942870.0
+	const LOWINT = 3325438800.0
+	const MEDIUMINT = 1233031547.0
+	const HIGHINT = 3420765524.0
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS,
+		"Music": MUSIC,
+		"Lowint": LOWINT,
+		"Mediumint": MEDIUMINT,
+		"Highint": HIGHINT
+	}
+
+class AUX_BUSSES:
+
+	const REVERB = 348963605.0
+
+	const _dict = {
+		"Reverb": REVERB
+	}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410.0
+	const NO_OUTPUT = 2317455096.0
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
